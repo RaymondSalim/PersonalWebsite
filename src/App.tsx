1045e35774ed2f443b69c4ce161ccb0b95c4c7ad
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2021-11-23 13:52:55 +0700

    Added icon contribution

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,29 @@ import {Email} from "./icons/Email";
 export default class App extends React.Component<any, AppState> {
   render() {
     return (
-      <div className={"flex items-center justify-center flex-col gap-y-4 bg-theme-secondary-dark h-screen"} >
-        <Logo classList={"h-24 w-24"}/>
-        <div className={"text-lg text-theme-secondary-light flex flex-col items-center"}>
-          <span className={"block"}>Site under construction.</span>
-          <span className={"block"}>Come back soon!</span>
+      <div className={"flex items-center justify-between flex-col bg-theme-secondary-dark h-screen"} >
+        <div></div>
+        <div className={"flex flex-col items-center gap-y-4"}>
+          <Logo classList={"h-24 w-24"}/>
+          <div className={"text-lg text-theme-secondary-light flex flex-col items-center"}>
+            <span className={"block"}>Site under construction.</span>
+            <span className={"block"}>Come back soon!</span>
+          </div>
+          <div className={"flex gap-x-4"}>
+            <a href="https://github.com/RaymondSalim" target="_blank" rel="noreferrer noopener" className={"text-white hover:text-theme-primary-hover"}>
+              <GitHub/>
+            </a>
+            <a href="mailto:raymond@raymonds.dev" target="_blank" rel="noreferrer noopener" className={"text-white hover:text-theme-primary-hover"}>
+              <Email/>
+            </a>
+          </div>
         </div>
-        <div className={"flex gap-x-4"}>
-          <a href="https://github.com/RaymondSalim" target="_blank" rel="noreferrer noopener" className={"text-white hover:text-theme-primary-hover"}>
-            <GitHub/>
-          </a>
-          <a href="mailto:raymond@raymonds.dev" target="_blank" rel="noreferrer noopener" className={"text-white hover:text-theme-primary-hover"}>
-            <Email/>
-          </a>
-        </div>
+        <footer className={"text-sm text-theme-secondary-light "}>
+          <div>
+            <span>Icons by </span>
+            <a target="_blank" rel="noreferrer noopener" href="https://colebemis.com/" className={"text-white hover:text-theme-primary-hover"}>Cole Bemis</a>
+          </div>
+        </footer>
       </div>
     );
   }
