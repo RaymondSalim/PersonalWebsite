231bacda8b7b8505e0fcbbbd72cff7f660794b11
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2021-12-04 11:41:03 +0700

    refactor(header): refactored code to prevent duplicates

--- src/navigation/Header.tsx
+++ src/navigation/Header.tsx
@@ -19,6 +19,12 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
   toggleOnScroll() {
     document.addEventListener('scroll', () => {
       let scrollPos = window.scrollY
+
+      // Limit scroll event to trigger only when change is greater than 5px
+      if (Math.abs(this.state.windowY - scrollPos) < 10) {
+        return
+      }
+
       let navMobile = document.querySelector(`#${NavbarMobile.elementID}`)
 
       let yLessThan90 = scrollPos < 90
@@ -42,7 +48,12 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
   render() {
     return (
       <header>
-        <NavbarMobile hamburger={this.props.navBarMobileProps.hamburger} menu={this.props.navBarMobileProps.menu}/>
+        <nav
+          id={NavbarMobile.elementID}
+          className="bg-theme-secondary-light dark:bg-theme-secondary-dark z-50 fixed w-full top-0  h-header flex flex-col justify-center transform transition-all duration-[250ms] px-4"
+        >
+          <NavbarMobile hamburger={this.props.navBarMobileProps.hamburger} menu={this.props.navBarMobileProps.menu}/>
+        </nav>
       </header>
     );
   }
