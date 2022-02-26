d555ffaa09708d2043d483da5a7bb457078518a5
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2022-02-26 15:05:21 +0700

    Squashed commit of the following:
    commit f272c3a35b9980c2ab8793fecba9524e72f8ca75
    Author: Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
    Date:   Sun Feb 20 22:47:46 2022 +0700
    
        fix(navbar): fix dark mode toggle on desktop is ignored when navigating by tab

--- src/util/darkmode/DarkModeToggle.tsx
+++ src/util/darkmode/DarkModeToggle.tsx
@@ -7,12 +7,20 @@ import BaseProps from "../../common/interface/BaseProps";
 export class DarkModeToggle extends React.Component<DarkModeToggleProps> {
   static localStorageKey = 'darkMode'
 
+  handleKeyPress = (e: React.KeyboardEvent) => {
+    const key = e.key.trim() || e.code
+    if (key == "Space" || key == "Enter") {
+      e.preventDefault();
+      this.props.onChange(e)
+    }
+  }
+
   render() {
     if (this.props.singleIconMode) {
       return (
-          <div className={"relative"} onClick={this.props.onChange}>
-            <Moon width={this.props.singleIconDim} height={this.props.singleIconDim} className={`${ this.props.isDarkModeEnabled ? 'opacity-100' : '-translate-y-full opacity-0'} absolute transition-all duration-300 hover:text-theme-primary-light dark:text-white dark:hover:text-theme-primary-light select-none`} />
-            <Sun width={this.props.singleIconDim} height={this.props.singleIconDim} className={`${ this.props.isDarkModeEnabled ? 'translate-y-full opacity-0' : 'opacity-100'} absolute transition-all duration-300 hover:text-theme-primary-light dark:text-white dark:hover:text-theme-primary-light select-none`} />
+          <div onKeyDown={this.handleKeyPress} tabIndex={0} className={`relative focus:text-theme-primary-light dark:focus:text-theme-primary-light focus:ring-0 hover:text-theme-primary-light dark:text-white dark:hover:text-theme-primary-light select-none`} onClick={this.props.onChange}>
+            <Moon width={this.props.singleIconDim} height={this.props.singleIconDim} className={`${ this.props.isDarkModeEnabled ? 'opacity-100' : '-translate-y-full opacity-0'} absolute transition-all duration-300 text-inherit`} />
+            <Sun width={this.props.singleIconDim} height={this.props.singleIconDim} className={`${ this.props.isDarkModeEnabled ? 'translate-y-full opacity-0' : 'opacity-100'} absolute transition-all duration-300 text-inherit`} />
           </div>
       )
     }
