90f42fd0a189735532014d9126fe3b1b379712d7
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2022-08-01 23:09:39 +0700

    fix(projects): updated projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -24,19 +24,19 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState> {
         name: 'Reddit Downloader',
         repoUrl: 'https://github.com/RaymondSalim/RedditDownloader-Kotlin',
         projUrl: '',
-        imgUrl: 'https://picsum.photos/1920',
+        imgUrl: 'https://raw.githubusercontent.com/RaymondSalim/CDN_Assets/main/Personal%20Website/Reddit%20Downloader.png',
         imgAlt: 'Reddit Downloader',
         techStacks: [
           'Kotlin',
         ],
-        description: 'An android app to easily download media files from reddit. Stores post information, supports background download. Utilizes tools & libraries such as WorkManager, RoomDB, Coroutines, etc.',
+        description: 'Easily download media files from reddit. Stores post information, supports background download. Utilizes tools & libraries such as WorkManager, RoomDB, Coroutines, etc.',
         date: new Date(2021, 3),
       },
       {
         name: 'E-commerce Web Scraper',
         repoUrl: 'https://github.com/RaymondSalim/FinalWebScrape',
         projUrl: '',
-        imgUrl: 'https://picsum.photos/1920',
+        imgUrl: 'https://raw.githubusercontent.com/RaymondSalim/CDN_Assets/main/Personal%20Website/WebScrape.jpeg',
         imgAlt: 'Web Scraper',
         techStacks: [
           'Python',
@@ -49,61 +49,74 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState> {
         name: 'Tracker',
         repoUrl: 'https://github.com/RaymondSalim/Tracker',
         projUrl: '',
-        imgUrl: 'https://picsum.photos/1920',
+        imgUrl: 'https://raw.githubusercontent.com/RaymondSalim/CDN_Assets/main/Personal%20Website/Tracker.png',
         imgAlt: 'Tracker',
         techStacks: [
           'Kotlin',
         ],
-        description: "Android app that logs and tracks user's location, call history, messages. Data is viewable from a different app. Uses Firebase for database and authenticcation. ",
+        description: "Logs and track user's location, call history, messages. Data is viewable from a seperate app. Uses Firebase for database and authentication. ",
         date: new Date(2020, 10),
       },
     ];
     // TODO! Add overlap between descriptions and image
     // TODO! Fix description color bg
+    // TODO! Parallax Scrolling
+    // TODO! Fix color (replace bg-gray-600)
     return (
-      <div id="project-grid" className={'flex flex-col gap-y-48'}>
+      <div id="project-grid" className={'flex flex-col gap-y-24 md:gap-y-36'}>
         {
           projects.map((proj, index) => {
             const descriptionContent = {
               __html: proj.description,
             };
 
-            return (<div key={proj.name} id={`project-${index}`} className={'group flex w-full odd:flex-row even:flex-row-reverse justify-between'}>
-              <img src={proj.imgUrl} alt={proj.imgAlt} style={{
-                aspectRatio: '16/9',
-                maxWidth: '30vw',
-              }}/>
-              <div id="project-desc-container" className={'flex flex-col justify-center gap-y-3 group-odd:text-right group-even:text-left child-no-pseudo'}>
-                <h3 className={'text-theme-primary-light dark:text-theme-primary-lighter'}>{proj.name}</h3>
-                <div className={'bg-gray-600 rounded-md p-6'}>
-                  <p className={'mb-4'} dangerouslySetInnerHTML={descriptionContent} />
+            return (
+              <div key={proj.name} id={`project-${index}`} className={'group w-full grid grid-cols-12 items-center'}>
+                <div
+                  className={'relative bp-max-768:h-full bg-theme-primary-light hover:bg-transparent transition-colors col-start-1 col-end-13 md:group-odd:col-start-1 md:group-odd:col-end-8 md:group-even:col-start-6 md:group-even:col-end-13 row-start-1 row-end-2 md:after:absolute md:after:w-full md:after:h-full md:after:content-[""] md:after:border md:after:top-0 md:after:left-0 z-0 md:group-odd:md:after:translate-x-4 md:group-even:md:after:-translate-x-4 md:after:translate-y-4 hover:md:group-odd:md:after:translate-x-0 hover:md:group-even:md:after:translate-x-0 hover:md:after:translate-y-0 md:after:transition-all md:after:-z-20'}
+                >
+                  <img
+                    src={proj.imgUrl}
+                    alt={proj.imgAlt}
+                    style={{
+                      aspectRatio: '16/9',
+                      // maxWidth: '25vw',
+                    }}
+                    className={'md:mix-blend-multiply bp-max-768:h-full bp-max-768:object-cover'}
+                  />
                 </div>
-                <ul className={'list-none'}>
-                  {proj.techStacks.map((el, ind) => (
-                    <li
-                      key={ind}
-                      className={'inline-block first-of-type:ml-0 last-of-type:mr-0 mx-4'}
-                    >{el}</li>
-                  ))}
-                </ul>
-                <div>
-                  {
-                    proj.repoUrl.length > 0
-                      ? <a href={proj.repoUrl} className={'inline-block hover:scale-125 text-white hover:text-theme-primary-light dark:hover:text-theme-primary-lighter transition-transform group-odd:first:ml-0 group-odd:last:mr-0 group-even:first:mr-0 group-even:last:ml-0 mx-2'}>
-                        <GitHub />
-                      </a>
-                      : null
-                  }
-                  {
-                    proj.projUrl.length > 0
-                      ? <a href={proj.projUrl} className={'inline-block hover:scale-125 text-white hover:text-theme-primary-light dark:hover:text-theme-primary-lighter transition-transform group-odd:first:ml-0 group-odd:last:mr-0 group-even:first:mr-0 group-even:last:ml-0 mx-2'}>
-                        <External />
-                      </a>
-                      : null
-                  }
+                <div id="project-desc-container" className={'bp-max-768:bg-gray-600 bp-max-768:bg-opacity-90 bp-max-768:p-10 col-start-1 col-end-13 md:group-odd:col-start-7 md:group-odd:col-end-13 md:group-even:col-start-1 md:group-even:col-end-7 row-start-1 row-end-2 md:group-odd:text-right md:group-even:text-left child-no-pseudo z-20'}>
+                  <h3 className={'text-theme-primary-light dark:text-theme-primary-lighter my-4'}>{proj.name}</h3>
+                  <div className={'md:bg-gray-600 rounded-md p-6 bp-max-768:px-0 my-4'}>
+                    <p className={'mb-4 text-white'} dangerouslySetInnerHTML={descriptionContent} />
+                  </div>
+                  <ul className={'list-none my-4'}>
+                    {proj.techStacks.map((el, ind) => (
+                      <li
+                        key={ind}
+                        className={'inline-block first-of-type:ml-0 last-of-type:mr-0 mx-4'}
+                      >{el}</li>
+                    ))}
+                  </ul>
+                  <div>
+                    {
+                      proj.repoUrl.length > 0
+                        ? <a href={proj.repoUrl} className={'inline-block hover:scale-125 text-white hover:text-theme-primary-light dark:hover:text-theme-primary-lighter transition-transform group-odd:first:ml-0 group-odd:last:mr-0 group-even:first:mr-0 group-even:last:ml-0 mx-2'}>
+                          <GitHub />
+                        </a>
+                        : null
+                    }
+                    {
+                      proj.projUrl.length > 0
+                        ? <a href={proj.projUrl} className={'inline-block hover:scale-125 text-white hover:text-theme-primary-light dark:hover:text-theme-primary-lighter transition-transform group-odd:first:ml-0 group-odd:last:mr-0 group-even:first:mr-0 group-even:last:ml-0 mx-2'}>
+                          <External />
+                        </a>
+                        : null
+                    }
+                  </div>
                 </div>
               </div>
-            </div>);
+            );
           })
         }
       </div>
